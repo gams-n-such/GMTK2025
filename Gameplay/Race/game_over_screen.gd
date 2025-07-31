75639afea11652cb4b761c215580a12c2effc24f
Denis Maximenko <manlok71@gmail.com>
2025-07-31 17:20:13 +0400

    Introduce win/lose logic to game over screen

--- Gameplay/Race/game_over_screen.gd
+++ Gameplay/Race/game_over_screen.gd
@@ -1,16 +1,10 @@
 extends Control
 
-var player_won
+var player_won : bool = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	%WinScreen.visible = player_won
+	%LoseScreen.visible = not player_won
 
 func _on_continue_button_pressed() -> void:
 	Game.return_to_main_menu()
