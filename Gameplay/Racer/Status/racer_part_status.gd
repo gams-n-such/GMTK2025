0dfc4b725cb8e3d9b7bae84b497e8d7fcff90e4e
Denis Maximenko <manlok71@gmail.com>
2025-08-02 12:27:46 +0400

    Implement RacerStatus screen

---
+++ Gameplay/Racer/Status/racer_part_status.gd
@@ -0,0 +1,50 @@
+class_name RacerPartStatusControl
+extends Control
+
+@export var part_type : RacerConfig.RACER_PART
+
+var racer : Racer:
+	get:
+		return racer
+	set(new_racer):
+		racer = new_racer
+		part = racer.get_part(part_type)
+
+var part : RacerPart:
+	get:
+		return part
+	set(new_part):
+		if part:
+			part.durability_changed.disconnect(_on_part_durability_changed)
+		part = new_part
+		if part:
+			part.durability_changed.connect(_on_part_durability_changed)
+
+var durability : float:
+	get:
+		return part.durability.value
+
+func _ready() -> void:
+	# HACK:
+	await get_tree().create_timer(0.5).timeout
+	racer = Game.player
+	update_visuals()
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_part_durability_changed(part: RacerPart, new_value: float) -> void:
+	update_visuals()
+
+var part_names : Dictionary[RacerConfig.RACER_PART, String] = {
+	RacerConfig.RACER_PART.RIDER: "Rider",
+	RacerConfig.RACER_PART.PONY: "Pony",
+	RacerConfig.RACER_PART.CHARIOT: "Cart",
+	RacerConfig.RACER_PART.WHEELS: "Wheels",
+	RacerConfig.RACER_PART.HORSESHOE: "H-shoes",
+}
+
+func update_visuals() -> void:
+	# TODO: use sprite + color
+	
+	%DurabilityLabel.text = "%s: %s" % [part_names[part_type], int(durability)]
--- Gameplay/Racer/racer_part.gd
+++ Gameplay/Racer/racer_part.gd
@@ -8,4 +8,10 @@ var durability : Attribute:
 		return %Durability
 
 # TODO: move decay speed here
-# TODO: separate attribute for max durability?
+# TODO: add separate attribute for max durability?
+
+# TODO: add old_value?
+signal durability_changed(part: RacerPart, new_value: float)
+
+func _on_durability_value_changed(attribute: Attribute, new_value: float) -> void:
+	durability_changed.emit(self, new_value)
