510fdee18ef89849693c72055ce4013d7fbe7573
Zorik <zorikwot@gmail.com>
2025-08-03 21:29:38 +0600

    added cart mini game

--- Gameplay/Mini Games/mini_game_cart.gd
+++ Gameplay/Mini Games/mini_game_cart.gd
@@ -1,4 +1,20 @@
 extends Node2D
 class_name MiniGameCart
 
-var dirt_left: float = 1.0
+var dirt_left: float = 100.0
+var mouse_down: bool = false
+var speed: float = 0.0
+
+func _process(delta: float) -> void:
+	%Sponge.global_position = get_global_mouse_position()
+	%Dirt.set_modulate(Color(Color.WHITE, dirt_left / 100))
+	dirt_left -= speed * delta / 10
+	speed = 0.0
+	if dirt_left < 0.0:
+		self.queue_free()
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	var mouse_event := event as InputEventMouseMotion
+	if mouse_event:
+		speed = mouse_event.screen_velocity.length()
